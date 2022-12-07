5848bd0a1ee065ae68c3355cef39695ccd6cc997
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2022-12-08 00:24:57 +0300

    Todo

--- satis/js/virtual_operations.js
+++ satis/js/virtual_operations.js
@@ -232,3 +232,15 @@ function UpdRow(pid, sid, is_virtual, qty, price, p_name, tax, discount_rate, ro
     hesapla("price", row_id)
 }
 
+
+
+function openHydrolic(id = "", row_id = "", tr = 0) {
+    var comp_id = document.getElementById("company_id").value;
+    var price_catid = document.getElementById("PRICE_CATID").value;
+    if (tr == 0) {
+        openBoxDraggable("index.cfm?fuseaction=product.emptypopup_virtual_main_partner&page=3&id=" + id + "&price_catid=" + price_catid + "&comp_id=" + comp_id + "&type=2&row_id=" + row_id)
+    } else {
+        openBoxDraggable("index.cfm?fuseaction=product.emptypopup_virtual_main_partner&page=3&id=" + id + "&price_catid=" + price_catid + "&comp_id=" + comp_id + "&type=2&row_id=" + row_id)
+    }
+
+}
