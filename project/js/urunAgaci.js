8d8cc8b8191c841e340e35f90204db964989d1bd
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2023-06-02 16:14:47 +0300

    proje ürün kayit

--- project/js/urunAgaci.js
+++ project/js/urunAgaci.js
@@ -390,6 +390,7 @@ function OpenBasketProducts(col = "", actType = "5") {
 }
 function newDraft() {
   var enmae = prompt("Ürün Adı");
+  if(enmae.length==0) return false;
   idA = 1000;
   console.log("Yeni Taslak");
   var d = document.createElement("div");
