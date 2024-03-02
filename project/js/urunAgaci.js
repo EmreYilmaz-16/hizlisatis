a6ea7c3be9a67661c3f2c2996f9524f23c169838
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-03-02 12:47:50 +0300

    proje ekleme düzenlendi

--- project/js/urunAgaci.js
+++ project/js/urunAgaci.js
@@ -2313,7 +2313,7 @@ function SetPrice2(idb, modal_id) {
     .getElementByIdb(idb)
     .setAttribute("data-netTotal", SP_FIYAT_HESAP_SONUC.Sp_Tutar);
 
-  document.getElementById("fiyatimis_"+idb).innerText=SP_FIYAT_HESAP_SONUC.SP_Fiyat
+  document.getElementById("fiyatimis_"+idb).innerText=SP_FIYAT_HESAP_SONUC.SP_Fiyat+" "+SP_FIYAT_HESAP_SONUC.SP_SelectedMoney
   console.log(SP_FIYAT_HESAP_SONUC);
   MaliyetHesapla2()
   closeBoxDraggable(modal_id);
