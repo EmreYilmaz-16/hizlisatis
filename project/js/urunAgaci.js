b562218799be79adeedbee8174b7f5ea8a5c7a45
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-05-16 17:14:58 +0300

    gemi makina ekle

--- project/js/urunAgaci.js
+++ project/js/urunAgaci.js
@@ -1970,7 +1970,7 @@ function MaliyetHesapla2() {
   });
   TTutar = commaSplit(TTutar);
   $("#maliyet").val(TTutar);
-MaliyetHesapla();
+//MaliyetHesapla();
 }
 function MaliyetHesapla() {
   var TotalPrice = 0;
